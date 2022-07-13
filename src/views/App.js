66adcfb9ca2191ca54ff12939fa146fe30e75111
quanganh <quanganh24112000@gmail.com>
2022-07-13 15:17:28 +0700

    commit

---
+++ src/views/App.js
@@ -0,0 +1,31 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import ListTodo from "./Todos/ListTodo";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Todos App</p>
+        <ListTodo />
+      </header>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
+}
+
+export default App;
